ef923c8fc426a425c675b0b2d4b7665a813db055
sameepvicky <sameepvicky@gmail.com>
2026-06-20 10:58:03 +0530

    chore: add baseline elisp header and draft readme

---
+++ org-commander.el
@@ -0,0 +1,33 @@
+;;; org-commander.el --- An Org-backed visual job orchestrator -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2026 sameepvicky <https://github.com/sameepvicky/>
+
+;; Author: sameepvicky <sameepvicky@gmail.com>
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "30.2") (org "9.6") (transient "0.5.0"))
+;; Keywords: convenience, tools, files
+;; URL: https://github.com/sameepvicky/org-commander
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; org-commander provides an Org-backed visual job orchestrator and batch
+;; processor, allowing users to execute, manage, and analyze complex file
+;; operations and shell scripts through a unified interface.
+
+;;; Code:
+
+(provide 'org-commander)
+;;; org-commander.el ends here
